c88b00d0bd3617acb058888a6fec70950f1a49c4
GokhanZengin07 <62075674+GokhanZengin07@users.noreply.github.com>
2022-05-23 22:44:25 +0300

    ExceptionHandlerMiddleware

---
+++ Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionHandlerMiddleware> logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate Next, ILogger<ExceptionHandlerMiddleware> Logger)
+        {
+            next = Next;
+            logger = Logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext) 
+        {
+            try
+            {
+                await next.Invoke(httpContext);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("hello");
+                logger.LogError(ex.Message);
+            }
+
+           
+        }
+    }
+}
